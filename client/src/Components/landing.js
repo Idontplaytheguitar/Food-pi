5a945b1e9a6e8e4d71e35398d7f577e629fe0cfe
Idontplaytheguitar <aguskenny@hotmail.com>
2021-09-23 04:51:02 -0300

    front wiiii

--- client/src/Components/landing.js
+++ client/src/Components/landing.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './styles/landing.module.css'
 
 export function Landing() {
   return (
-    <div>  
-            <Link to="/home"><h1>Home</h1></Link>
+    <div className={styles.Landing}>  
+            <Link to="/home"><p>Home</p></Link>
     </div>
   )
 };
