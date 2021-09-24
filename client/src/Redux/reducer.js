f4096393ad4cbf357e431b308e42de6ab6368693
Idontplaytheguitar <aguskenny@hotmail.com>
2021-09-24 03:47:18 -0300

    Starting redux

---
+++ client/src/Redux/reducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+    cards: [],
+    loadingd: false,
+    currentPage:1,
+    cardsPerPage: 9,
+  };
+
+export function Reducer (state = initialState, action){
+    switch (action.type) {
+        case "":
+            return{
+                
+            }
+        case "":
+            return {
+             
+            }
+        case "":
+            return{
+            
+            }
+        case "":
+
+        default: return state
+    }
+}
---
+++ client/src/Redux/store.js
@@ -0,0 +1,5 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { Reducer }from "./reducer";
+import thunk from "redux-thunk";
+
+export const store = createStore(Reducer,applyMiddleware(thunk) )
